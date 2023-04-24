17ab73649a096225af0d2ef0ad384435e5a59d96
Ben Fields <fieldsben5@gmail.com>
2023-04-23 22:02:24 -0700

    Setup and some member functs

--- mandelbrot/ComplexPlane.cpp
+++ mandelbrot/ComplexPlane.cpp
@@ -1,2 +1,60 @@
 #include "ComplexPlane.h"
 
+ComplexPlane::ComplexPlane(float aspectRatio)
+{
+	m_aspectRatio = aspectRatio;
+	m_view.setSize(BASE_WIDTH, -BASE_HEIGHT * m_aspectRatio);
+	m_view.setCenter(0.0, 0.0);
+	m_zoomCount = 0;
+}
+
+
+void ComplexPlane::zoomIn()
+{
+	m_zoomCount++;
+
+	float x = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
+	float y = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
+
+	m_view.setSize(x, y);
+}
+
+
+// zoomOut()
+
+
+// setCenter()
+
+
+// getView()
+
+
+// setMouseLocation()
+
+
+// loadText()
+
+
+size_t ComplexPlane::countIterations(Vector2f coord)
+{
+	complex<double> c = sqrt(pow(coord.x, 2) + pow(coord.y, 2));
+	complex<double> z = 0.0;
+
+	size_t iterations = 0;
+
+	for (; iterations < MAX_ITER; iterations++)
+	{
+		z = pow(z, 2) + c;
+
+		// break condition
+		if (abs(z) > 2.0)
+		{
+			break;
+		}
+	}
+
+	return iterations;
+}
+
+
+// iterationsTORGB()
--- mandelbrot/main.cpp
+++ mandelbrot/main.cpp
@@ -1,12 +1,60 @@
 // Assignment: Mandelbrot Set
-//
 // Names: Aubrey Fields, William Garcia-Cortes
 
+#include "ComplexPlane.h"
 #include <iostream>
 
+using namespace sf;
 using namespace std;
 
 int main()
 {
+	// Init values
+	unsigned int width = VideoMode::getDesktopMode().width;
+	unsigned int height = VideoMode::getDesktopMode().height;
+	float aspectRatio = static_cast<float>(height) / width;
+
+	VideoMode vm(width, height);
+	RenderWindow window(vm, "Mandelbrot Set Plotter");
+
+	Font font;
+	if (!font.loadFromFile("./fonts/Comic_Sans_MS.ttf"))
+	{
+		cout << "Failed to find font..." << endl;
+	}
+
+	Text information;
+	information.setFont(font);
+	information.setCharacterSize(18);
+
+	// Main loop:
+	while (window.isOpen())
+	{
+		/*
+		****************************************
+		Handle the players input
+		****************************************
+		*/
+
+
+
+		/*
+		****************************************
+		Update the scene
+		****************************************
+		*/
+
+
+
+		/*
+		****************************************
+		Draw the scene
+		****************************************
+		*/
+
+
+
+	} // End main loop
+
 	return 0;
 }
